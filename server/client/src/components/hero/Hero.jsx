55140d3e6b4e85993738911b7afe8fff7fbfa121
Parrot <greggambugua@gmail.com>
2024-04-19 15:09:42 -0700

    Update Hero.jsx

--- server/client/src/components/hero/Hero.jsx
+++ server/client/src/components/hero/Hero.jsx
@@ -25,18 +25,14 @@ const HeroSection = () => {
     <Slider {...settings}>
       {/* First Slide with only background image */}
       <div className="relative py-20 bg-cover bg-center bg-no-repeat bg-fixed bg-blend-overlay h-screen flex items-center">
-        <img
-          src={backgroundImage1}
-          alt="Background 1"
-          className="absolute inset-0 object-cover w-full h-full"
-        />
+        <img src={backgroundImage1} alt="Background 1" className="absolute inset-0 object-cover w-full h-full" />
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 max-w-5xl mx-auto px-4 text-center text-white font-mono">
           <div className="flex flex-col h-full justify-center">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
-              Welcome to Medplus: Connecting Patients with Trusted Medical Professionals
-            </h1>
-            <p className="text-base md:text-lg lg:text-xl mb-8">
+          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-4">
+            Welcome to Medplus: Connecting Patients with Trusted Medical Professionals
+          </h1>
+            <p className="text-lg mb-8">
               Experience expert advice and personalized treatment from the comfort of your home.
             </p>
             <div>
@@ -53,20 +49,16 @@ const HeroSection = () => {
 
       {/* Second Slide with background image and text */}
       <div className="relative py-20 bg-cover bg-center bg-no-repeat bg-fixed bg-blend-overlay h-screen flex items-center">
-        <img
-          src={backgroundImage2}
-          alt="Background 2"
-          className="absolute inset-0 object-cover w-full h-full"
-        />
-
+        <img src={backgroundImage2} alt="Background 2" className="absolute inset-0 object-cover w-full h-full" />
+        
         {/* Text and buttons */}
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="absolute inset-0 max-w-5xl mx-auto px-4 text-center text-white font-mono">
           <div className="flex flex-col h-full justify-center">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-4">
-              Join Our Network of Medical Professionals
-            </h1>
-            <p className="text-base md:text-lg lg:text-xl mb-8">
+          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mb-4">
+            Join Our Network of Medical Professionals
+          </h1>
+            <p className="text-lg mb-8">
               Connect, collaborate, and share knowledge to enhance overall health.
             </p>
             <div>
@@ -85,3 +77,4 @@ const HeroSection = () => {
 };
 
 export default HeroSection;
+
