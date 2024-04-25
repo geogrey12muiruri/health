c9046fee3155d5d87460697a48e69927f59b842e
Parrot <greggambugua@gmail.com>
2024-04-25 07:40:43 -0700

    Update PostCard.jsx

--- server/client/src/components/PostCard.jsx
+++ server/client/src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef,  useState } from "react";
+import React, { useRef,  useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
 import { NoProfile } from "../assets";
