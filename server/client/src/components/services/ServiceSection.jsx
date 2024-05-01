312d4b8cba2219ce22b9276a4f9b08d869e63819
Parrot <greggambugua@gmail.com>
2024-05-01 15:10:34 +0300

    Update ServiceSection.jsx

--- server/client/src/components/services/ServiceSection.jsx
+++ server/client/src/components/services/ServiceSection.jsx
@@ -28,7 +28,7 @@ const ServicesSection = () => {
   ];
 
   return (
-    <section className="bg-blue-100 py-12 font-mono">
+    <section className="bg-blue-100 bg-opacity-25 py-12 font-mono">
       <div className="max-w-7xl mx-auto px-4">
         <h2 className="text-3xl font-bold text-center mb-8">Our Services</h2>
         <motion.div
