34c3aebd6554da48dff3d05c91a82a3516637874
Parrot <greggambugua@gmail.com>
2024-05-02 12:34:23 +0300

    Update CollaborationSection.jsx

--- server/client/src/components/collaborations/CollaborationSection.jsx
+++ server/client/src/components/collaborations/CollaborationSection.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BsHouseDoor, BsPlusSquare, BsSearch, BsChatDots } from "react-icons/bs";
 import collaborationImage from "../../assets/collaboration.jpg";
 
+const BottomNavigation = () => {
+  return (
+    <nav className="fixed bottom-0 left-0 right-0 bg-white shadow flex justify-around items-center py-2">
+      <Link to="/" className="text-2xl text-gray-500">
+        <BsHouseDoor />
+      </Link>
+      <Link to="/add-post" className="text-2xl text-gray-500">
+        <BsPlusSquare />
+      </Link>
+      <Link to="/search" className="text-2xl text-gray-500">
+        <BsSearch />
+      </Link>
+      <Link to="/chats" className="text-2xl text-gray-500">
+        <BsChatDots />
+      </Link>
+    </nav>
+  );
+};
 
 const CollaborationHeroSection = () => {
   return (
@@ -15,13 +35,8 @@ const CollaborationHeroSection = () => {
           <p className="text-lg mb-8">Delivering Quality Training to Our Learners Worldwide</p>
         </div>
 
-        {/* Video Section */}
-        <div className="container mx-auto px-4 pb-20">
-          <div className="text-center">
-            <h2 className="text-2xl font-bold mb-4">Watch Our Collaboration in Action</h2>
-            
-          </div>
-        </div>
+        {/* Bottom Navigation */}
+        <BottomNavigation />
       </div>
     </section>
   );
