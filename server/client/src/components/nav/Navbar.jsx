80e58d05d6197e82e672b9037dcc01d17ee7417c
Parrot <greggambugua@gmail.com>
2024-05-02 08:34:18 +0300

    Update Navbar.jsx

--- server/client/src/components/nav/Navbar.jsx
+++ server/client/src/components/nav/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-blue-950 shadow-lg font-sans text-white sticky top-0 z-50">
+    <nav className="bg-blue-600 shadow-lg font-sans text-white sticky top-0 z-50">
       <div className="max-w-7xl mx-auto px-4 py-2">
         <div className="flex items-center justify-between">
           {/* Logo */}
