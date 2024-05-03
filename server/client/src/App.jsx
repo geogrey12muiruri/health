b199e2b4f9d01b175ba5b012549e95c3fb3405fc
Parrot <greggambugua@gmail.com>
2024-05-03 09:39:22 +0300

    Update App.jsx

--- server/client/src/App.jsx
+++ server/client/src/App.jsx
@@ -5,7 +5,7 @@ import { HomePage, Login, Profile, Register, ResetPassword } from "./pages";
 import Contact from './components/contact/Contact';
 import Navbar from './components/nav/Navbar';
 import Footer from './components/footer/Footer';
-import Pillars from './components/pillars/Pillars';
+import Pillars from './components/pillars/PillarsSection';
 import Socials from "./pages/Socials";
 import SymptomCheaker from "./components/carriculum/Carriculum";
 
