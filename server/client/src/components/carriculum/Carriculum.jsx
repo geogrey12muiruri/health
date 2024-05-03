74181544e63dd072be2fe99b979d66df59bc203b
Parrot <greggambugua@gmail.com>
2024-05-04 00:19:46 +0300

    Update Carriculum.jsx

--- server/client/src/components/carriculum/Carriculum.jsx
+++ server/client/src/components/carriculum/Carriculum.jsx
@@ -20,38 +20,39 @@ const ConnectDoctorComponent = ({ userName, profileImage }) => {
   };
 
   return (
-    <div className="bg-blue-500 p-4 rounded-lg shadow-md">
-      <div className="flex items-center justify-between mb-2">
-        <div>
-          <h3 className="text-lg font-bold text-white">How are you feeling?</h3>
+<div className="bg-gradient-to-br from-blue-500 to-white p-4 rounded-lg shadow-md">
+  <div className="flex items-center justify-between mb-2">
+    <div>
+      <h3 className="text-lg font-bold text-white">How are you feeling?</h3>
       <p className="text-white">Let's connect you to a doctor.</p>
     </div>
-        {profileImage && (
-          <img src={profileImage} alt="User profile" className="w-10 h-10 rounded-full" />
-        )}
-      </div>
-      <div className="mb-2">
-        <input
-          type="text"
-          value={symptoms}
-          onChange={handleInputChange}
-          placeholder="Enter your symptoms (separated by commas)"
-          className="w-full h-8 border border-gray-300 rounded-md p-2"
-        />
-      </div>
-      <button
-        onClick={handleSearch}
-        className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
-      >
-        Search
-      </button>
-      {recommendation && (
-        <div className="mt-2">
-          <h4 className="text-md font-bold mb-1">Recommended Doctor:</h4>
-          <p>{recommendation}</p>
-        </div>
-      )}
+    {profileImage && (
+      <img src={profileImage} alt="User profile" className="w-10 h-10 rounded-full" />
+    )}
+  </div>
+  <div className="mb-2">
+    <input
+      type="text"
+      value={symptoms}
+      onChange={handleInputChange}
+      placeholder="Enter your symptoms (separated by commas)"
+      className="w-full h-8 border border-gray-300 rounded-md p-2 text-gray-800"
+    />
+  </div>
+  <button
+    onClick={handleSearch}
+    className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+  >
+    Search
+  </button>
+  {recommendation && (
+    <div className="mt-2">
+      <h4 className="text-md font-bold mb-1">Recommended Doctor:</h4>
+      <p>{recommendation}</p>
     </div>
+  )}
+</div>
+
   );
 };
 
