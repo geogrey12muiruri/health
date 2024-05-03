5c2eceeaeb5c67ce11cf1e6913e7c23fd8cba9e8
Parrot <greggambugua@gmail.com>
2024-05-03 23:31:43 +0300

    Update Pillars.jsx

--- server/client/src/components/pillars/Pillars.jsx
+++ server/client/src/components/pillars/Pillars.jsx
@@ -6,7 +6,7 @@ import SymptomChecker from '../../components/carriculum/Carriculum';
 
 const PillarsSection = () => {
   return (
-    <section className="py-10 bg-blue-200">
+    <section className="py-10 bg-gray-50">
       <div className="container mx-auto px-4">
         <div className="mb-8">
           <h2 className="text-3xl font-bold text-center mb-6">Health Services</h2>
