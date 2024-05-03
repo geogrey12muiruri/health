5e1339bdba05c4e6e90f05e168c13dffde7b453f
Gidy30B <154205415+Gidy30B@users.noreply.github.com>
2024-05-03 08:19:18 +0300

    Update index.js

--- server/dbConfig/index.js
+++ server/dbConfig/index.js
@@ -1,16 +1,21 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const dbConnection = async () => {
     try {
-       const connection = await mongoose.connect(process.env.MONGO, {
+        const mongoURI = process.env.MONGO;
+
+        const connection = await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            
         });
 
         console.log('Database connected successfully');
     } catch (error) {
-        console.log('Error in connecting to database', error);
+        console.error('Error connecting to database', error);
     }
-    }
-    export default dbConnection;
+}
+
+export default dbConnection;
