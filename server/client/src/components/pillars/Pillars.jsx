913487ca2cc359281d29a492bb81e62a3ac32ae4
Parrot <greggambugua@gmail.com>
2024-05-03 13:57:27 +0300

    Update Pillars.jsx

--- server/client/src/components/pillars/Pillars.jsx
+++ server/client/src/components/pillars/Pillars.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { FaHome, FaEnvelope, FaSearch, FaCalendar } from 'react-icons/fa';
+import { FaHome, FaEnvelope, FaSearch, FaCalendar, FaAmbulance, FaUserMd, FaFlask } from 'react-icons/fa';
 
 const PillarsSection = () => {
   return (
