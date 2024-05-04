65be644d8c51d7eb9da8d63adef4d77ca3f3a275
Parrot <greggambugua@gmail.com>
2024-05-04 09:57:20 +0300

    Update index.js

--- server/client/src/utils/index.js
+++ server/client/src/utils/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { SetPosts } from '../redux/postSlice.js';
 
-const API_URL = "https://health-3-olid.onrender.com";
+const API_URL = "https://health-8.onrender";
 
 
 
